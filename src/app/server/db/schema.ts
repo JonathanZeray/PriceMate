e873c336da0c814d821c0bf790881587eb04d69f
Jonathan Zeray <jonathan.zeray@hotmail.com>
2024-07-24 13:32:59 +0200

    First drizzle schema to get all products done
    Co-authored-by: Kenny Dinh <kenny.t.dinh93@gmail.com>
    Co-authored-by: Robin Sahin <robin.sahin88@gmail.com>
    Co-authored-by: Magdalena Karpinska <magdalena.karpinska13@gmail.com>

---
+++ src/app/server/db/schema.ts
@@ -0,0 +1,45 @@
+// Example model schema from the Drizzle docs
+// https://orm.drizzle.team/docs/sql-schema-declaration
+
+import { sql } from "drizzle-orm";
+import {
+  index,
+  pgTable,
+  serial,
+  timestamp,
+  varchar,
+  real,
+} from "drizzle-orm/pg-core";
+
+/**
+ * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
+ * database instance for multiple projects.
+ *
+ * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
+ *
+ *
+ */
+
+export const products = pgTable(
+  "image",
+  {
+    id: serial("id").primaryKey(),
+    productName: varchar("productName", { length: 256 }).notNull(),
+    imageUrl: varchar("imageUrl", { length: 1024 }).notNull(),
+    userId: varchar("userId", { length: 256 }).notNull(),
+    initialPrice: real("initialPrice").notNull().default(0),
+    secondHandPrice: real("secondHandPrice").notNull().default(0),
+    category: varchar("category", { length: 256 }).notNull(),
+    condition: varchar("condition", { length: 256 }).notNull(),
+    age: real("age").notNull().default(0),
+    createdAt: timestamp("created_at", { withTimezone: true })
+      .default(sql`CURRENT_TIMESTAMP`)
+      .notNull(),
+    updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
+      () => new Date()
+    ),
+  },
+  (example) => ({
+    nameIndex: index("name_idx").on(example.productName),
+  })
+);
