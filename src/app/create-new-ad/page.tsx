b8586375b61eca267869a72abf4c95c3a76c0a18
Jonathan Zeray <jonathan.zeray@hotmail.com>
2024-07-25 17:03:47 +0200

    Responsive styling and styling on prompt login page
    Co-authored-by: Kenny Dinh <kenny.t.dinh93@gmail.com>
    Co-authored-by: Robin Sahin <robin.sahin88@gmail.com>
    Co-authored-by: Magdalena Karpinska <magdalena.karpinska13@gmail.com>

--- src/app/create-new-ad/page.tsx
+++ src/app/create-new-ad/page.tsx
@@ -1,12 +1,20 @@
-import { Form } from '@/components/form';
-import { SignedIn, SignedOut } from '@clerk/nextjs';
-import { auth } from '@clerk/nextjs/server';
+import { Form } from "@/components/form";
+import { SignedIn, SignedOut, SignInButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
 
 export default function CreateNewAd() {
   return (
     <>
       <SignedOut>
-        <h1>Please sign in to create a new ad!</h1>
+        <div className="flex h-[40vh] md:h-[80vh] justify-center items-center">
+          <h3 className="text-bold text-lg md:text-3xl">
+            Please{" "}
+            <span className="border-b-2 border-black font-bold">
+              <SignInButton />
+            </span>{" "}
+            to create a new ad!
+          </h3>
+        </div>
       </SignedOut>
       <SignedIn>
         <Form userId={auth().userId!} />
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -6,13 +6,13 @@ export const Navbar = () => {
   return (
     <nav className="w-full text-2xl font-extrabold my-6 flex justify-between items-center px-4 md:px-16">
       <h1 className="p-4">
-        <Link href="/" className="border-b-2 border-green-800">
+        <Link href="/" className="border-b-[3px] border-green-800">
           PriceMate
         </Link>
       </h1>
       <SignedOut>
         <SignInButton>
-          <span className="border-2 border-black px-2 rounded-lg text-md cursor-pointer font-normal">
+          <span className="border-2 border-black px-4 rounded-lg text-base cursor-pointer font-normal">
             Sign in
           </span>
         </SignInButton>
