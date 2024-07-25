6110986a4c31de80640a5ec51d017edaa424e78f
r0binsahin <robin.sahin88@gmail.com>
2024-07-25 09:40:35 +0200

    get prod by id in queries
    Co-authored-by: magdalena-karpinska <magdalena.karpinska13@gmail.com>
    Co-authored-by: JonathanZeray <jonathan.zeray@hotmail.com>
    Co-authored-by: iiiamken <kenny.t.dinh93@gmail.com>

--- src/app/server/queries.ts
+++ src/app/server/queries.ts
@@ -1,7 +1,17 @@
 import { db } from './db';
 import * as schema from './db/schema';
+import { eq } from 'drizzle-orm';
 
 export const getAllProducts = async () => {
   const products = await db.select().from(schema.products);
   return products;
+};
+
+export const getProductById = async (id: number) => {
+  const productById = await db
+    .select()
+    .from(schema.products)
+    .where(eq(schema.products.id, id));
+
+  return productById[0];
 };
