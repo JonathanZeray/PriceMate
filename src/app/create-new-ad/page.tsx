299160be25efdeceb7cff866aa3b53ac611c44e3
r0binsahin <robin.sahin88@gmail.com>
2024-07-25 14:52:02 +0200

    create ad wrapped in signin
    Co-authored-by: magdalena-karpinska <magdalena.karpinska13@gmail.com>
    Co-authored-by: JonathanZeray <jonathan.zeray@hotmail.com>
    Co-authored-by: iiiamken <kenny.t.dinh93@gmail.com>

--- src/app/create-new-ad/page.tsx
+++ src/app/create-new-ad/page.tsx
@@ -1,5 +1,15 @@
 import { Form } from '@/components/form';
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs';
 
 export default function CreateNewAd() {
-  return <Form />;
+  return (
+    <>
+      <SignedOut>
+        <h1>Please sign in to create a new ad!</h1>
+      </SignedOut>
+      <SignedIn>
+        <Form />
+      </SignedIn>
+    </>
+  );
 }
